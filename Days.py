b78b7a8f0ed710486f5febba9bfed026956bb022
ArthurrMrv <arthur.morvan@student-cs.fr>
2023-12-10 13:57:08 +0100

    first commit

---
+++ Days.py
@@ -0,0 +1,221 @@
+import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Days:
+    def __init__(self) -> None:
+        self.data = dict()
+        self.cities = set()
+        
+    def addDay(self, day):
+        self.cities.add(day.location)
+        self.data[f"{day.getDate()}|{day.getLocation()}"] = day
+        
+    def getDay(self, date, location):
+        return self.data[f"{date}|{location}"]
+    
+    def getDays(self):
+        return self.data.values()
+    
+    def getCityWeather(self, city_name):
+        return [day for day in self.getDays() if day.getLocation() == city_name.lower()]
+    
+    def getAvgTemperature(self, city_name):
+        city_weather = self.getCityWeather(city_name)
+        return sum([day.getMaxTemp() for day in city_weather]) / len(city_weather) if len(city_weather) > 0 else None
+    
+    def getPrecipitation(self, city_name):
+        city_weather = self.getCityWeather(city_name)
+        return sum([day.getPrecipitation() for day in city_weather]) if len(city_weather) > 0 else None
+    
+    def getMaxWindSpeed(self, city_name):
+        city_weather = self.getCityWeather(city_name)
+        return max([day.getWindSpeed() for day in city_weather]) if len(city_weather) > 0 else None
+    
+    def getMinWindSpeed(self, city_name):
+        city_weather = self.getCityWeather(city_name)
+        return min([day.getWindSpeed() for day in city_weather]) if len(city_weather) > 0 else None
+    
+    def getMedianTemperature(self, city_name):
+        city_weather = self.getCityWeather(city_name)
+        return sorted([day.getMaxTemp() for day in city_weather])[len(city_weather) // 2] if len(city_weather) > 0 else None
+    
+    def getTotalPrecipitation(self, city_name):
+        city_weather = self.getCityWeather(city_name)
+        return sum([day.getPrecipitation() for day in city_weather]) if len(city_weather) > 0 else None
+    
+
+    def categorizeDay(self, city_name, date, t = 0.2):
+        
+        if city_name.lower() not in self.cities:
+            return None
+        
+        avgTemp = self.getAvgTemperature(city_name.lower())
+        day_temp = self.getDay(date, city_name.lower()).getAverageTemp()
+        tolerance = t*avgTemp
+        
+        if avgTemp - tolerance < day_temp < avgTemp + tolerance:
+            return "Moderate"
+        elif avgTemp > avgTemp + tolerance:
+            return "Warm"
+        else:
+            return "Cold"
+    
+    def hotestDay(self, city_name):
+        city_weather = self.getCityWeather(city_name)
+        return max(city_weather, key=lambda day: day.getMaxTemp()) if len(city_weather) > 0 else None
+    
+    def coldestDay(self, city_name):
+        city_weather = self.getCityWeather(city_name)
+        return min(city_weather, key=lambda day: day.getMinTemp()) if len(city_weather) > 0 else None
+
+    def correlationMatrix(self, city_name):
+        city_weather = self.getCityWeather(city_name) 
+        if city_weather == []:
+            return None
+        elif city_weather[0].getCo2Levels() == None:
+            matrix = np.array([[day.getMaxTemp(), day.getMinTemp(), day.getWindSpeed(), day.getHumidity(), day.getCloudCover(), day.getPrecipitation()] for day in city_weather])
+        matrix = np.array([[day.getMaxTemp(), day.getMinTemp(), day.getWindSpeed(), day.getHumidity(), day.getCloudCover(), day.getPrecipitation(), day.getCo2Levels(), day.getSeaLevelRise()] for day in city_weather])
+        return np.corrcoef(matrix, rowvar=False)
+    
+    def correlationMatrix(self, city_name):
+        city_weather = self.getCityWeather(city_name)
+        if city_weather == []:
+            return None
+        elif city_weather[0].getCo2Levels() is None:
+            matrix = np.array([[day.getMaxTemp(), day.getMinTemp(), day.getWindSpeed(), day.getHumidity(), day.getCloudCover(), day.getPrecipitation()] for day in city_weather])
+            titles = ['Max Temp', 'Min Temp', 'Wind Speed', 'Humidity', 'Cloud Cover', 'Precipitation']
+        else:
+            matrix = np.array([[day.getMaxTemp(), day.getMinTemp(), day.getWindSpeed(), day.getHumidity(), day.getCloudCover(), day.getPrecipitation(), day.getCo2Levels(), day.getSeaLevelRise()] for day in city_weather])
+            titles = ['Max Temp', 'Min Temp', 'Wind Speed', 'Humidity', 'Cloud Cover', 'Precipitation', 'CO2 Levels', 'Sea Level Rise']
+
+        correlation_matrix = np.corrcoef(matrix, rowvar=False)
+        return correlation_matrix, titles
+    
+    def plotCorrelationMatrix(self, *city_names):
+        for city_name in city_names:
+            matrix, titles = self.correlationMatrix(city_name)
+            plt.matshow(matrix)
+            plt.xticks(range(len(titles)), titles, rotation=45)
+            plt.yticks(range(len(titles)), titles)
+            plt.title(f'Correlation Matrix for {city_name.capitalize()}')
+            plt.colorbar()
+            plt.show()
+        
+    
+    def loadTemp(self, filename):
+        with open(filename, 'r') as file:
+            lines = file.readlines()[1:]
+            for line in lines:
+                line = line.strip().split(',')
+                date = line[0]
+
+                location = line[1]
+                maxTemp = float(line[2])
+                minTemp = float(line[3])
+                precipitation = float(line[4])
+                windSpeed = float(line[5])
+                humidity = float(line[6])
+                cloudCover = float(line[7])
+                co2Levels = float(line[8]) if len(line) > 8 else None
+                seaLevelRise = float(line[9]) if len(line) > 9 else None
+                self.cities.add(location.lower())
+                self.addDay(Day(date, location, maxTemp, minTemp, precipitation, windSpeed, humidity, cloudCover, co2Levels, seaLevelRise))
+    
+    def plotTemperature(self, *city_names):
+        for city_name in city_names:
+            city_weather = self.getCityWeather(city_name)
+            plt.plot([day.getDate() for day in city_weather], [day.getMaxTemp() for day in city_weather], label = city_name)
+        #affiche labels
+        plt.legend()
+        
+        #put legent box outside of graph
+        plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+
+        plt.xticks(rotation=45)
+        plt.xlabel('Date')
+        plt.ylabel('Temperature (°C)')
+        plt.title('Temperature over time')
+        plt.show()
+    
+    def plotAvgTemperature(self, *city_names):
+        for city_name in city_names:
+            city_weather = self.getCityWeather(city_name)
+            plt.bar(city_name.capitalize(), self.getAvgTemperature(city_name), label = city_name)
+        
+        plt.xticks(rotation=45)
+        plt.xlabel(f'City')
+        plt.ylabel('Temperature (°C)')
+        plt.title('Average Temperature')
+        plt.show()
+        
+    #add data by loading a file
+    def loadTemp(self, filename):
+        with open(filename, 'r') as file:
+            lines = file.readlines()[1:]
+            for line in lines:
+                line = line.strip().split(',')
+                #date = datetime.datetime.strptime(line[0], '%Y-%m-%d')
+                date = line[0]
+
+                location = line[1]
+                maxTemp = float(line[2])
+                minTemp = float(line[3])
+                precipitation = float(line[4])
+                windSpeed = float(line[5])
+                humidity = float(line[6])
+                cloudCover = float(line[7])
+                co2Levels = float(line[8]) if len(line) > 8 else None
+                seaLevelRise = float(line[9]) if len(line) > 9 else None
+                self.cities.add(location.lower())
+                self.addDay(Day(date, location, maxTemp, minTemp, precipitation, windSpeed, humidity, cloudCover, co2Levels, seaLevelRise))
+    
+class Day:
+    
+    def __init__(self, date, location, maxTemp, minTemp, precipitation, windSpeed, humidity, cloudCover, co2Levels = None, seaLevelRise = None):
+        self.date = date
+        self.location = location.lower()
+        self.maxTemp = maxTemp
+        self.minTemp = minTemp
+        self.precipitation = precipitation
+        self.windSpeed = windSpeed
+        self.humidity = humidity
+        self.cloudCover = cloudCover
+        self.co2Levels = co2Levels
+        self.seaLevelRise = seaLevelRise
+    
+    def getDate(self):
+        return self.date
+    
+    def getLocation(self):
+        return self.location
+    
+    def getMaxTemp(self):
+        return self.maxTemp
+    
+    def getMinTemp(self):
+        return self.minTemp
+    
+    def getPrecipitation(self):
+        return self.precipitation
+    
+    def getWindSpeed(self):
+        return self.windSpeed
+    
+    def getHumidity(self):
+        return self.humidity
+    
+    def getCloudCover(self):
+        return self.cloudCover
+    
+    def getCo2Levels(self):
+        return self.co2Levels
+    
+    def getSeaLevelRise(self):
+        return self.seaLevelRise
+    
+    def getAverageTemp(self):
+        return (self.maxTemp + self.minTemp) / 2
+    
+    def __str__(self):
+        return "Date: " + str(self.date) + "\nLocation: " + str(self.location) + "\nMax Temp: " + str(self.maxTemp) + "\nMin Temp: " + str(self.minTemp) + "\nPrecipitation: " + str(self.precipitation) + "\nWind Speed: " + str(self.windSpeed) + "\nHumidity: " + str(self.humidity) + "\nCloud Cover: " + str(self.cloudCover) + "\nCO2 Levels: " + str(self.co2Levels) + "\nSea Level Rise: " + str(self.seaLevelRise) + "\n"
---
+++ Readme.txt
@@ -0,0 +1,29 @@
+# Weather Analysis Project
+
+## Overview
+
+This Python project focuses on analyzing weather data for multiple cities. The dataset includes information such as maximum and minimum temperatures, precipitation, wind speed, humidity, and cloud cover. The goal is to gain insights into the weather patterns of different cities and explore the impact of climate change on a specific city.
+
+## Project Structure
+
+The project is organized into the following components:
+
+1. **`data/`**: This directory contains the input dataset (`temperature_data.csv`).
+
+2. **`src/`**: This directory contains the Python source code for the project.
+
+3. **`results/`**: This directory stores the results and visualizations generated during the analysis.
+
+4. **Run the Analysis Script:**
+    ```bash
+    python src/weather_analysis.py argv1 argv2 argv3
+
+    ```
+5. argv1 = ..., argv2 = ... etc.
+	
+## Results
+
+The visualizations and results of the analysis will be saved in the `results/` directory.
+
+
+
